bf3106fc6c351f1f85599b6e5b597df54aa700a0
HayashiMitsu <hm20011027@gmail.com>
2021-06-23 11:35:33 +0900

    sanctumインストール&環境構築

--- laravel-react/resources/js/app.js
+++ laravel-react/resources/js/app.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './components/Example';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Auth from './components/Auth';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/Navbar';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Route path='/about' component={About} />
+            <Route path='/auth' component={Auth} />
+            <Route exact path='/' component={Home} />
+        </BrowserRouter>
+    )
+}
 
 if (document.getElementById('app')) {
-    ReactDOM.render(<Example />,
+    ReactDOM.render(<App />,
     document.getElementById('app'));
 }
 
