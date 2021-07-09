88eb266b16d5b9b61bfc0d3a40813e985069a08a
waonpad <waonpad@gmail.com>
2021-07-09 19:22:34 +0900

    てすと

---
+++ laravel-react/app/Http/Controllers/MakeworkspaceController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Workspace;
+
+class MakeworkspaceController extends Controller
+{
+    public function store(Request $request)
+    {
+    	$name = $request['name'];
+    	$user_id = $request['user_id'];
+
+    	$workspace = new Workspace;
+    	$workspace -> name = $name;
+    	$workspace -> user_id = $user_id;
+    	$workspace -> save();
+    	$res = Workspace::select('id')->where('user_id', $user_id)->first();
+    	$id = $res->id;
+
+    	return response()->json($id);
+    }
+
+    public function update(Request $request)
+    {
+    	$name = $request['name'];
+    	$user_id = $request['user_id'];
+    	$id = $request['id'];
+
+    	App\Workspace::where('id', $id)
+    		->update(['name' => $name, 'user_id' => $user_id]);
+    }
+
+    public function dalete(Request $request)
+    {
+    	$id = $request['id'];
+
+    	Workspace::find($id)->delete();
+    }
+}
--- laravel-react/routes/api.php
+++ laravel-react/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserRegist;
+use App\Http\Controllers\MakeworkspaceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,5 +21,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/user', [UserRegist::class, 'index']);
+
+
+
+Route::post('/Makeworkspace', [MakeworkspaceController::class, 'store']);
+Route::post('/Makeworkspace', [MakeworkspaceController::class, 'update']);
+Route::post('/Makeworkspace', [MakeworkspaceController::class, 'delete']);
 
 Route::post('/userCreate', [UserRegist::class, 'store']);
