8479955f6d8b548d35222ef446bb2646ed5dffae
github ryuichi3811 <ryuichi19633811@gmail.com>
2021-07-15 10:18:13 +0900

    デフォルトのデザインに変更

--- laravel-react/resources/js/components/pages/Shiftcalendar.jsx
+++ laravel-react/resources/js/components/pages/Shiftcalendar.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import BackgroundCalendar from '../atom/BackgroundCalendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function Shiftcalendar() {
+    const [value, onChange] = useState(new Date())
+    // getFormatDate(date) {
+    //     `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(-2)}${('0' + date.getDate()).slice(-2)}`
+    // }
+
     return (
         <BackgroundCalendar>
             <div className='calendarContainer'>
-                <Calendar/>
+                <Calendar
+                    onChange={onChange}
+                    locale="ja-JP"
+                    value={value}
+                />
             </div>
         </BackgroundCalendar>
     )
