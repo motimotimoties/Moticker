00a0282c00f52b3303a756ebb13fbdf25969bb67
HayashiMitsu <hm20011027@gmail.com>
2021-08-25 23:14:55 +0900

    fix:時間登録用のコントローラーを修正

--- laravel-react/app/Http/Controllers/ShiftregistController.php
+++ laravel-react/app/Http/Controllers/ShiftregistController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Undecided_shift;
+use App\Models\Workspace;
+use Illuminate\Support\Facades\DB;
 
 class ShiftregistController extends Controller
 {
+    public function index(Request $request)
+    {
+        
+    }
+
     public function store(Request $request)
     {
         $user_id = $request['user_id'];
@@ -15,22 +22,19 @@ class ShiftregistController extends Controller
         $enter_time = $request['enter_time'];
         $exit_time = $request['exit_time'];
 
-        $workspace_id = $request['workspace_id'];
-        $woid = User::select('id')->where('workspace_id', $workspace_id)->first();
+        // $workspace_id = $request['workspace_id'];
+        // $woid = User::select('id')->where('workspace_id', $workspace_id)->first();
 
-        if(!$woid){
+        $undecided_shift = new undecided_shift;
+        $undecided_shift->user_id = $user_id;
+        $undecided_shift->date = $date;
+        $undecided_shift->enter_time = $enter_time;
+        $undecided_shift->exit_time = $exit_time;
+        $undecided_shift->save();
+        // $res = undecided_shift::select('id')->where('user_id', $user_id)->first();
+        $id = $undecided_shift->id;
 
-            $undecided_shift = new undecided_shift;
-            $undecided_shift->user_id = $user_id;
-            $undecided_shift->date = $date;
-            $undecided_shift->enter_time = $enter_time;
-            $undecided_shift->exit_time = $exit_time;
-            $undecided_shift->save();
-            // $res = undecided_shift::select('id')->where('user_id', $user_id)->first();
-            $id = $undecided_shift->id;
-
-            return response()->json($id);
-        }
+        return response()->json($id);
     }
 
     public function update(Request $request)
