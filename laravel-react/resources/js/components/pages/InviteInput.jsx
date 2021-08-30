e1a510b795de5a18fb14dc4c5baa8a7c7678d5b0
HayashiMitsu <hm20011027@gmail.com>
2021-08-30 18:07:57 +0900

    fix:すべてのページに要素のみ追加

---
+++ laravel-react/resources/js/components/pages/InviteInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function InviteInput() {
+    return (
+        <div>
+            <form>
+                <input type="email" name="email" />
+            </form>
+        </div>
+    )
+}
---
+++ laravel-react/resources/js/components/pages/LoginInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LoginInput() {
+    return (
+        <div>
+            <form>
+                <input type="email" name="email" />
+            </form>
+        </div>
+    )
+}
