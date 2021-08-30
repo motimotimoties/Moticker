6e643f7d48745a915fe07fc5461d81557f3b4ce5
HayashiMitsu <hm20011027@gmail.com>
2021-08-30 21:24:10 +0900

    fix:controller修正

--- laravel-react/app/Http/Controllers/UndecidedShift.php
+++ laravel-react/app/Http/Controllers/UndecidedShift.php
@@ -15,8 +15,8 @@ class UndecidedShift extends Controller
         $users_id = $request['users_id'];
         $workspace_users = DB::table('users')->join('workspaces', 'users.id', '=', 'workspaces.user_id')->join('undecided_shifts','undecided_shifts.user_id', '=', 'users.id')->select('workspaces.name', 'workspaces.id as workspaces_id', 'users.id as users_id', 'undecided_shifts.date', 'undecided_shifts.enter_time', 'undecided_shifts.exit_time')->where('users.id', $users_id)->get();
         // $undecided_shift = Undecided_shift::where('user_id', $users_id)->get();
-        $workspace_users['enter_time'] = substr($workspace_users->enter_time, 0, 5);
-        $workspace_users['exit_time'] = substr($workspace_users->exit_time, 0, 5);
+        // $workspace_users['enter_time'] = substr($workspace_users->enter_time, 0, 5);
+        // $workspace_users['exit_time'] = substr($workspace_users->exit_time, 0, 5);
         return response()->json($workspace_users);
     }
 }
