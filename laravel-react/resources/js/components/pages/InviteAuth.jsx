266280ee4a28887d3610bd8156ca2c9fae663ec8
ryuichi3811 <81604427+ryuichi3811@users.noreply.github.com>
2021-08-31 21:18:18 +0900

    invite/inputのCSSかいた～ (#53)
    Co-authored-by: ryuichi3811 <ryuichi3811@gmail.com>

--- laravel-react/resources/js/components/pages/InviteAuth.jsx
+++ laravel-react/resources/js/components/pages/InviteAuth.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 export default function InviteAuth() {
     return (
-        <div>
-            <form>
-                <input type="text" maxLength="6" name="token" autoFocus />
-            </form>
-        </div>
+        <BackgroundDesign>
+        <BackgroundDesign>
+            <div className="AuthContainer">
+                <div className="AuthWrap">
+                    <p className="mTi">
+                        メールでコードを確認する<br/>
+                        <span className="long">
+                        6 文字のコードを送信しました。このコードの有効期間は短いため、すぐに入力してください。
+                        </span>
+                    </p>
+                    <form>
+                        <input type="text" maxLength="6" name="token" autoFocus placeholder="000000" />
+                    </form>
+                    <p className="mTi">
+                        <span>コードが見つからない？迷惑メールフォルダを確認してみてください！</span>
+                    </p>
+                </div>
+            </div>
+        </BackgroundDesign>
+        </BackgroundDesign>
     )
 }
--- laravel-react/resources/js/components/pages/InviteInput.jsx
+++ laravel-react/resources/js/components/pages/InviteInput.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import BackgroundDesign from '../atom/BackgroundDesign';
+import GoogleIcon from '../../../images/google-logo.png';
 
 export default function InviteInput() {
     return (
-        <div>
-            <form>
-                <input type="email" name="email" />
-                <input type="submit"></input>
-            </form>
+        <BackgroundDesign>
+        <div className="inputContainer">
+            <div className="inputWrap">
+                <p className="mTi">
+                    まずは、メールアドレス<br/>を入力してください<br/>
+                    <span>
+                        仕事用のメールアドレスがおススメです。
+                    </span>
+                </p>
+                <Link className="googleLogin" to="#">
+                    <span><img className="GoogleIcon" src={GoogleIcon} alt="GoogleIcon"/></span>
+                    googleで続行する
+                </Link>
+                <div className="mOr">
+                    <hr /><p>あるいは</p><hr />
+                </div>
+                <form>
+                    <input type="email" name="email"  placeholder="メールアドレス"/>
+                    <input type="submit" value="認証メールを送信"></input>
+                </form>
+            </div>
         </div>
+        </BackgroundDesign>
     )
 }
